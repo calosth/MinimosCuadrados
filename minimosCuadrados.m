53818b72d8aa6b271bce1851c51b3f0e8d3c6593
Carlos Linares <cadalisa@gmail.com>
2014-07-14 18:09:13 -0430

    RK45 add

--- minimosCuadrados.m
+++ minimosCuadrados.m
@@ -1,133 +1,143 @@
-function minimosCuadrados(vecx , vecy, method)
+function minimosCuadrados()
+    % Programa que calcula la ecuación de la recta mediante Mínimos Cuadrados
+    % utilizando siete tipos de cambios de variables, además 
+    % sugiere cual es la mejor función usando el criterio del menor error y grafica 
+    % todos las funciones calculadas contrastadas con los puntos dados.
+    %
+    % La función lee un archivo de texto que tiene en cada linea pares de
+    % puntos
+    %
+    %
+    % Ejemplo: minimosCuadrados()
     
-    % Programa que calcula la ecuacion de la recta mediante mínimos
-    % cuadrados
+    file = fopen('archivo.txt');
     
-    % Se ingresan los valores de x separados por comas y distintos entre sí
-    % Se ingresan los valores de y separados por comas
+    x = fgets(file);
+    vecx = str2num(x);
+     y = fgets(file);
+    vecy = str2num(y);
+    
+    Orinigal_vecx = vecx;
+    Orinigal_vecy = vecy;
     
 
     if length(vecx) == length(vecy)
-    
-        switch method
-
-            case 1
-                
-                for i=1:length(vecx)
-                    vecx(i) = 1 / (vecx(i));
-                end
-                
-                [a,b] = AyB(vecx,vecy);
-                
-                func = sprintf('%.4f/x + %.4f',a,b);  
-                
-                for i=1:length(vecx)
-                    vecx(i) = 1 / (vecx(i));
-                end
-                
-            
-            case 2
-                 
-                 for i=1:length(vecx)
-                     vecx(i) = vecx(i) * vecy(i);
-                 end
-                 
-                 [a,b] = AyB(vecx,vecy);
-                 
-                 d = 1 / a; 
-                 
-                 c = d * b;
-                 
-                 func = sprintf('%.4f / x + %.4f',d,c); 
-                 
-                 for i=1:legnth(vecx)
-                    
-                     vecx(i) = vecx(i) / vecy(i);
-                     
-                 end
-                 
-
-            case 3
-                
-                %En caso de que la funcion sea Potencial
-                for i=1:length(vecx)
-                    vecx(i) = log(vecx(i));
-                    vecy(i) = log(vecy(i));
-                end
-                [a,b] = AyB(vecx,vecy);
-
-                c = exp(b);   
-
-                func = sprintf('%.4f*x.^(%.4f)',c,a);
-
-                for i=1:length(vecx)
-                    vecx(i) = exp(vecx(i));
-                    vecy(i) = exp(vecy(i));        
-                end
-                
-            case 4 
-                for i=1:length(vecx)
-                    vecy(i) = log(vecy(i));
-                end
-                [a,b] = AyB(vecx,vecy);
-                c = exp(b);
-                func = sprintf('e^(%.4f*x)', a);
-                
-                for i=1:legnth(vecx)
-                   
-                    vecy(i) = exp(vecy(i));
-                    
-                end
-                
-            case 5
-                for i=1:length(vexc)
-                    vecx(i) = log(vecx(i))
-                end
-                [a,b] = AyB(vecx,vecy);
-                func = sprintf('%.4fLn(x) + %.4f',a,b); 
-                
-                for i=1:length(vexc)
-                    vecx(i) = exp(vecx(i))
-                end                 
-             case 6
-                 for i=1;length(vecx)
-                     vecy(i) = - sqrt(vecy(i));
-                 end
-                 [a,b] = AyB(vecx,vecy);
-                 func = sprintf('(%.4f*x + %.4f) ^ -2',a,b);
-                 for i=1;length(vecx)
-                     vecy(i) = -(vecy(i))^2;
-                 end                
-            
-             case 7
-
-                %En caso de que la funcion sea lineal
-
-                [a,b] = AyB(vecx,vecy);
-                func = sprintf('%.4f*x + (%.4f)',a,b);
-            
-
-        end
-        E = 0
-        for i=1:length(vecx)
-            x = vecx(i);
-            fx(i) = eval(func);
-            E = E + ( vecy(i) - fx(i) )^2;
-        end
+        fprintf('Funcion                             Error Local\n');
+        fprintf('-----------------------------------------------\n');
+        ErrorMenor = 1000000;
+        ktotal=0;
         
+        for i=1:length(vecx)
+            vecx(i) = 1 / (vecx(i));
+        end
+        [a,b] = AyB(vecx,vecy);
+        func = sprintf('%.4f/x + %.4f',a,b);         
+        [E,ErrorMenor,ktotal] = GraphicAndError(ktotal,ErrorMenor, Orinigal_vecx, Orinigal_vecy, func,1);
+        fprintf('\t\t\t%.4f\n',E);
+        vecx = Orinigal_vecx;
+        vecy = Orinigal_vecy;
+        
+        
+        for i=1:length(vecx)
+            vecx(i) = vecx(i) * vecy(i);
+        end
+        [a,b] = AyB(vecx,vecy);
+        d = 1 / a;
+        c = d * b;
+        func = sprintf('%.4f / x + %.4f',d,c);                                 
+        [E,ErrorMenor,ktotal] = GraphicAndError(ktotal,ErrorMenor, Orinigal_vecx, Orinigal_vecy, func,2);
+        fprintf('\t\t\t%.4f\n',E);
+        vecx = Orinigal_vecx;
+        vecy = Orinigal_vecy;        
 
-        plot(vecx,fx,'b'), hold on;
 
-        plot(vecx,vecy,'.');
-
-        legend(func,'puntos');
-
-        hold off; 
+        for i=1:length(vecx)
+            vecx(i) = log(vecx(i));
+            vecy(i) = log(vecy(i));
+        end
+        [a,b] = AyB(vecx,vecy);
+        c = exp(b);   
+        func = sprintf('%.4f*x.^(%.4f)',c,a);        
+        [E,ErrorMenor,ktotal] = GraphicAndError(ktotal,ErrorMenor, Orinigal_vecx, Orinigal_vecy, func,3);
+        fprintf('\t\t\t%.4f\n',E);
+        vecx = Orinigal_vecx;
+        vecy = Orinigal_vecy;        
+        
+        
+        for i=1:length(vecx)
+            vecy(i) = log(vecy(i));
+        end
+        [a,b] = AyB(vecx,vecy);
+        c = exp(b);
+        func = sprintf('exp((%.4f*x))', a);
+        [E,ErrorMenor,ktotal] = GraphicAndError(ktotal,ErrorMenor, Orinigal_vecy, Orinigal_vecy, func,4);
+        fprintf('\t\t\t%.4f\n',E);
+        vecx = Orinigal_vecx;
+        vecy = Orinigal_vecy;
+        
+        
+        for i=1:length(vecx)
+            vecx(i) = log(vecx(i));
+        end
+        [a,b] = AyB(vecx,vecy);
+        func = sprintf('%.4f*log(x) + %.4f',a,b);
+        [E,ErrorMenor,ktotal] = GraphicAndError(ktotal,ErrorMenor, Orinigal_vecx, Orinigal_vecy, func,5);
+        fprintf('\t\t%.4f\n',E);                
+        vecx = Orinigal_vecx;
+        vecy = Orinigal_vecy;
+        
+        
+        for i=1:length(vecx)
+            vecy(i) = - sqrt(vecy(i));
+        end
+        [a,b] = AyB(vecx,vecy);
+        func = sprintf('(%.4f*x + %.4f) ^ -2',a,b);
+        [E,ErrorMenor,ktotal] = GraphicAndError(ktotal,ErrorMenor, Orinigal_vecx, Orinigal_vecy, func,6);
+        fprintf('\t\t%.4f\n',E);
+        vecx = Orinigal_vecx;
+        vecy = Orinigal_vecy;        
+        
+        [a,b] = AyB(vecx,vecy);
+        func = sprintf('%.4f*x + (%.4f)',a,b);     
+        [E,ErrorMenor,ktotal] = GraphicAndError(ktotal,ErrorMenor, Orinigal_vecx, Orinigal_vecy, func,7);
+        fprintf('\t\t\t%.4f\n',E);         
+        vecx = Orinigal_vecx;
+        vecy = Orinigal_vecy;  
+        
+        fprintf('-----------------------------------------------\n');        
+        fprintf('La función que mejor se ajusta a los puntos dados es %d, con error local: %.4f\n\n',ktotal,ErrorMenor)
     
     else        
         fprintf('No tienen la misma longitud los vectores x e y');
         
     end
+    
+end
+    
+function [E,ErrorMenor,ktotal] = GraphicAndError(ktotal,ErrorMenor,vecx,vecy, func,k)
+
+    E = 0;
+    for m=1:length(vecx)
+        x = vecx(m);
+        fx(m) = eval(func);
+        E = E + (vecy(m) - fx(m))^2; % Calculo el error
+    end
+
+    fprintf('%d) %s',k,func);
+    
+    subplot(3,4,k,'Position');
+    
+    plot(vecx,fx,'b-',vecx,vecy,'.r'), hold on;
+    title(['Error: ',num2str(E)]);
+    legend(func,'Location','Best');
+    hold off;    
+
+    if(E < ErrorMenor)
+        ErrorMenor = E;
+        ktotal = k;
+    end
+     
+end
     
     
     
---
+++ rkf45p.m
@@ -0,0 +1,77 @@
+function [y1] = rkf45p(f,a,b,in,hmin,hmax,tol)
+
+    format long;
+    
+    BAND = 1;
+    X(1) = a;
+    Z(1) = in;
+    Y(1) = in;
+    auxx=0;
+    
+    R = 0;   
+    
+    h = 0;
+    
+    i = 1;
+    
+    fprintf(' %.7f      %.7f    %.7f     %.7f     %.7f \n\n\n',X(1),Z(1),h,R,Y(1));
+    
+    h = hmax;
+    
+    
+    while(BAND)
+        
+        x = X(i);
+        y = Y(i);
+        
+        k1 = h * eval(f);
+        
+        auxx = x;
+        auxy = y;              
+        
+        x = auxx + (h / 4);
+        y = auxy + (k1 / 4);
+        
+        k2 = h * eval(f);
+        
+        x = auxx + (h * 3/8);
+        y = auxy + ((3/32 * k1) + (9/32 * k2));
+        
+        k3 = h * eval(f);
+        
+        x = auxx + (12/13 * h);
+        y = auxy + ((1932/2197 * k1) - (7200/2197 * k2) + (7296/2197 * k3));
+        
+        k4 = h * eval(f);
+        
+        x = auxx + h;
+        y = auxy + ((439/216 * k1) - (8*k2) + (3680/513 * k3) - (845/4104 * k4));
+        
+        k5 = h * eval(f);
+        
+        x = auxx + (1/2 * h);
+        y = auxy + (-(8/27 * k1) + (2*k2) - (3544/2565 * k3) + (1859/4104 * k4) - (11/40 * k5));
+        
+        k6 = h * eval(f);       
+        
+        R = abs( ( k1/360 ) - ( 128*k3/4275 ) - ( 2197*k4/75240 ) + ( k5/50 ) + ( 2*k6/55 ) ) / h ;
+        
+        if R < tol
+            
+            auxx = auxx + h;
+            
+            Z(i+1) = Z(i) + ( (25/216 * k1) + (1408/2565 * k3) + (2197/4104 * k4) - (1/5 * k5) );
+            
+            Y(i+1) = Y(i) + ( ( 16/135 * k1 ) + ( 6656/12825 * k3 ) + ( 28561/56430 * k4 ) - ( 9/50 * k5 ) + ( 2/55 * k6 ) );
+            
+            fprintf(' %.7f      %.7f    %.7f     %.7f     %.7f',h,Z(i),auxx,R,Y(i));
+            
+        end
+        
+        i = i + 1;
+        X(i) = X(i-1) + auxx;
+
+        
+    end
+
+end
