0dee68562edf6084d86fb6c2f4277ba6e683e5df
walis85300 <walojose46@gmail.com>
2014-07-16 22:37:20 -0430

    Concidiones y demás

--- rkf45p.m
+++ rkf45p.m
@@ -14,7 +14,7 @@ function [y1] = rkf45p(f,a,b,in,hmin,hmax,tol)
     
     i = 1;
     
-    fprintf(' %.7f      %.7f    %.7f     %.7f     %.7f \n\n\n',X(1),Z(1),h,R,Y(1));
+    fprintf(' %.7f      %.7f    %.7f     %.7f     %.7f \n',X(1),Z(1),h,R,Y(1));
     
     h = hmax;
     
@@ -37,7 +37,7 @@ function [y1] = rkf45p(f,a,b,in,hmin,hmax,tol)
         x = auxx + (h * 3/8);
         y = auxy + ((3/32 * k1) + (9/32 * k2));
         
-        k3 = h * eval(f);
+        k3 = h * eval(f);m , 
         
         x = auxx + (12/13 * h);
         y = auxy + ((1932/2197 * k1) - (7200/2197 * k2) + (7296/2197 * k3));
@@ -56,21 +56,42 @@ function [y1] = rkf45p(f,a,b,in,hmin,hmax,tol)
         
         R = abs( ( k1/360 ) - ( 128*k3/4275 ) - ( 2197*k4/75240 ) + ( k5/50 ) + ( 2*k6/55 ) ) / h ;
         
-        if R < tol
+        if R <= tol
             
             auxx = auxx + h;
             
-            Z(i+1) = Z(i) + ( (25/216 * k1) + (1408/2565 * k3) + (2197/4104 * k4) - (1/5 * k5) );
-            
+            Z(i+1) = Z(i) + ( (25/216 * k1) + (1408/2565 * k3) + (2197/4104 * k4) - (1/5 * k5) );  
             Y(i+1) = Y(i) + ( ( 16/135 * k1 ) + ( 6656/12825 * k3 ) + ( 28561/56430 * k4 ) - ( 9/50 * k5 ) + ( 2/55 * k6 ) );
             
-            fprintf(' %.7f      %.7f    %.7f     %.7f     %.7f',h,Z(i),auxx,R,Y(i));
+            fprintf(' %.7f      %.7f    %.7f     %.7f     %.7f\n',auxx,Z(i+1),h,R,Y(i+1));
             
         end
         
         i = i + 1;
-        X(i) = X(i-1) + auxx;
-
+        X(i) = auxx;
+        
+        d = 0.84*((tol/R)^(1/4));
+        
+        if (d <= 0.1)
+            h = 0.1 * h;            
+        elseif (d >= 4)
+            h = 4 * h;            
+        else
+            h = d * h;
+        end
+        
+        if (h > hmax)
+            h = hmax;   
+        end
+        
+        if (auxx >= b)
+            BAND = 0;
+        elseif ((auxx + h) > b)
+            h = b - auxx;
+        elseif (h < hmin)
+            BAND = 0;
+            break;
+        end                                            
         
     end
 
